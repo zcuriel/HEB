45cc680e476decc0ade4804916defc1b6fd89eec
zcuriel <zcuriel@hotmail.com>
2019-04-25 12:53:49 -0500

    Authenticate user

---
+++ HEB.NetGiphyA/Controllers/AuthorizeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HEB.NetGiphyA.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        public IActionResult Login(string returnUrl)
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult Logout(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        public IActionResult Cancel() => RedirectToAction("Index", "Home");
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+        }
+    }
+}
